36706a3280e398d01318708e1436d4e5b3d64a10
Arda Eymen Ulus <ulusarda9@gmail.com>
2025-07-12 20:24:30 +0300

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,24 +4,18 @@ import cors from "cors";
 import dotenv from "dotenv";
 import todoRoutes from "./routes/todoRoutes";
 
-// .env dosyasını yükle
 dotenv.config();
 
-// Express uygulaması
 const app = express();
 
-// Ortam değişkenlerinden PORT ve MONGO_URI'yi al
 const PORT = process.env.PORT || 3000;
 const MONGO_URI = process.env.MONGO_URI as string;
 
-// Middleware'ler
 app.use(cors());
 app.use(express.json());
 
-// Routes
 app.use("/", todoRoutes);
 
-// MongoDB bağlantısı ve sunucunun başlatılması
 mongoose
   .connect(MONGO_URI)
   .then(() => {
